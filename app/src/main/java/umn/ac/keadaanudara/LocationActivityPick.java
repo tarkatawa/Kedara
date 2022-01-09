534647eac1031c2a98eccb970974c52027e1111a
fikriachmadi <fikriachmadi.fa@gmail.com>
2022-01-09 14:18:48 +0700

    lat - lon

---
+++ app/src/main/java/umn/ac/keadaanudara/LocationActivityPick.java
@@ -0,0 +1,174 @@
+package umn.ac.keadaanudara;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.SearchView;
+
+import com.google.android.gms.location.LocationRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
+import umn.ac.keadaanudara.Adapter.CityAdapter;
+import umn.ac.keadaanudara.Main.InputOneTimeActivity;
+import umn.ac.keadaanudara.Main.LocationActivity;
+import umn.ac.keadaanudara.Main.MainActivity;
+import umn.ac.keadaanudara.Model.City;
+
+public class LocationActivityPick extends AppCompatActivity {
+    private List<City> cityArrayList = new ArrayList<>();
+    private CityAdapter cityAdapter = null;
+    private static final String SHARED_PREF = "SHARED_PREF";
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+    LocationRequest locationRequest;
+    private static final int PERMISSION_FINE_LOCATION = 99;
+    private static final String KABKO_KEY = "KABKO";
+    private static final String LAT_KEY = "LAT";
+    private static final String LON_KEY = "LON";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location_pick);
+        getSupportActionBar().setTitle("Change Location");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        sharedPreferences = getSharedPreferences(SHARED_PREF, MODE_PRIVATE);
+
+        ImageView imgLocateMe = findViewById(R.id.imgBGLocateMe);
+
+        loadCity();
+
+        initListener();
+
+        imgLocateMe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View views) {
+                editor = sharedPreferences.edit();
+                editor.putString(KABKO_KEY, null);
+                editor.putString(LAT_KEY, "0.0");
+                editor.putString(LON_KEY, "0.0");
+                editor.apply();
+                Intent intent = new Intent(LocationActivityPick.this, InputOneTimeActivity.class);
+                startActivity(intent);
+            }
+        });
+
+//        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
+//        recyclerView.addItemDecoration(dividerItemDecoration);
+
+    }
+
+    private String readJSONDataFromFile() throws IOException {
+
+        InputStream inputStream = null;
+        StringBuilder stringBuilder = new StringBuilder();
+
+        try {
+            String jsonString;
+            inputStream = getResources().openRawResource(R.raw.list_location_id);
+            BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+            while ((jsonString = bufferedReader.readLine()) != null) {
+                stringBuilder.append(jsonString);
+            }
+        } finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        }
+        return new String(stringBuilder);
+    }
+
+    private void loadCity() {
+        try {
+            String jsonString = readJSONDataFromFile();
+            JSONArray jsonArray = new JSONArray(jsonString);
+
+            for (int i = 0; i<jsonArray.length(); ++i) {
+                JSONObject zeroJsonObject = jsonArray.getJSONObject(i);
+                City cities = new City();
+                cities.setKabko(zeroJsonObject.getString("kabko"));
+                cities.setLat(zeroJsonObject.getDouble("lat"));
+                cities.setLon(zeroJsonObject.getDouble("lon"));
+                cityArrayList.add(cities);
+            }
+
+            RecyclerView recyclerView = findViewById(R.id.locationRecycler);
+            recyclerView.setHasFixedSize(true);
+            RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+            recyclerView.setLayoutManager(layoutManager);
+
+            cityAdapter = new CityAdapter(this, cityArrayList);
+            recyclerView.setAdapter(cityAdapter);
+            cityAdapter.notifyDataSetChanged();
+
+        } catch (JSONException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater locationMenuInflater = getMenuInflater();
+        locationMenuInflater.inflate(R.menu.menu, menu);
+
+        MenuItem locationMenuItem = menu.findItem(R.id.menuSearch);
+        SearchView locationSearchView = (SearchView) locationMenuItem.getActionView();
+
+        locationSearchView.setQueryHint("Search City");
+        locationSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                cityAdapter.getFilter().filter(s);
+                return false;
+            }
+        });
+        locationMenuItem.getIcon().setVisible(false, false);
+
+        return true;
+
+    }
+
+    public void initListener() {
+        cityAdapter.setOnItemClickListener((view, position) -> {
+            City changeCity = cityArrayList.get(position);
+            Intent intent = new Intent(LocationActivityPick.this, InputOneTimeActivity.class);
+            intent.putExtra("KABKO", changeCity.getKabko());
+            intent.putExtra("LAT", Double.valueOf(changeCity.getLat()));
+            intent.putExtra("LON", Double.valueOf(changeCity.getLon()));
+//            intent.putExtra("CONDITION", true);
+//            editor = sharedPreferences.edit();
+//            editor.putString(KABKO_KEY, changeCity.getKabko());
+//            editor.putString(LAT_KEY, String.valueOf(changeCity.getLat()));
+//            editor.putString(LON_KEY, String.valueOf(changeCity.getLon()));
+//            editor.apply();
+//            Intent intent = new Intent(LocationActivityPick.this, InputOneTimeActivity.class);
+            startActivity(intent);
+        });
+    }
+}
--- app/src/main/java/umn/ac/keadaanudara/Main/InputRepetitive.java
+++ app/src/main/java/umn/ac/keadaanudara/Main/InputRepetitive.java
@@ -109,85 +109,113 @@ public class InputRepetitive extends AppCompatActivity {
                     WeeklyMondayActivityModel weeklyMondayActivityModel;
                     if (etActivity.length() != 0 && etLocation.length() != 0 && etTime.length() != 0 && etReminder.length() != 0) {
                         weeklyMondayActivityModel = new WeeklyMondayActivityModel(etActivity.getText().toString(), etLocation.getText().toString(), etTime.getText().toString(), Integer.parseInt(etReminder.getText().toString()));
+                        WeeklyMondayDatabaseHelper weeklyMondayDatabaseHelper = new WeeklyMondayDatabaseHelper(InputRepetitive.this);
+                        boolean success = weeklyMondayDatabaseHelper.addOne(weeklyMondayActivityModel);
+                        Intent intent = new Intent(InputRepetitive.this, ListActivity.class);
+                        startActivity(intent);
                         Toast.makeText(InputRepetitive.this, weeklyMondayActivityModel.toString(), Toast.LENGTH_SHORT).show();
+                        finish();
                     }else{
                         Toast.makeText(InputRepetitive.this, "Fields cannot be empty", Toast.LENGTH_SHORT).show();
-                        weeklyMondayActivityModel = new WeeklyMondayActivityModel("NaN", "NaN", "NaN", 0);
+//                        weeklyMondayActivityModel = new WeeklyMondayActivityModel("NaN", "NaN", "NaN", 0);
                     }
-                    WeeklyMondayDatabaseHelper weeklyMondayDatabaseHelper = new WeeklyMondayDatabaseHelper(InputRepetitive.this);
-                    boolean success = weeklyMondayDatabaseHelper.addOne(weeklyMondayActivityModel);
+
                 }
                 if(cbTuesday.isChecked()){
                     WeeklyTuesdayActivityModel weeklyTuesdayActivityModel;
                     if (etActivity.length() != 0 && etLocation.length() != 0 && etTime.length() != 0 && etReminder.length() != 0) {
                         weeklyTuesdayActivityModel = new WeeklyTuesdayActivityModel(etActivity.getText().toString(), etLocation.getText().toString(), etTime.getText().toString(), Integer.parseInt(etReminder.getText().toString()));
+                        WeeklyTuesdayDatabaseHelper weeklyTuesdayDatabaseHelper = new WeeklyTuesdayDatabaseHelper(InputRepetitive.this);
+                        boolean success = weeklyTuesdayDatabaseHelper.addOne(weeklyTuesdayActivityModel);
+                        Intent intent = new Intent(InputRepetitive.this, ListActivity.class);
+                        startActivity(intent);
                         Toast.makeText(InputRepetitive.this, weeklyTuesdayActivityModel.toString(), Toast.LENGTH_SHORT).show();
+                        finish();
                     }else{
                         Toast.makeText(InputRepetitive.this, "Fields cannot be empty", Toast.LENGTH_SHORT).show();
-                        weeklyTuesdayActivityModel = new WeeklyTuesdayActivityModel("NaN", "NaN", "NaN", 0);
+//                        weeklyTuesdayActivityModel = new WeeklyTuesdayActivityModel("NaN", "NaN", "NaN", 0);
                     }
-                    WeeklyTuesdayDatabaseHelper weeklyTuesdayDatabaseHelper = new WeeklyTuesdayDatabaseHelper(InputRepetitive.this);
-                    boolean success = weeklyTuesdayDatabaseHelper.addOne(weeklyTuesdayActivityModel);
+
                 }
                 if(cbWednesday.isChecked()){
                     WeeklyWednesdayActivityModel weeklyWednesdayActivityModel;
                     if (etActivity.length() != 0 && etLocation.length() != 0 && etTime.length() != 0 && etReminder.length() != 0) {
                         weeklyWednesdayActivityModel = new WeeklyWednesdayActivityModel(etActivity.getText().toString(), etLocation.getText().toString(), etTime.getText().toString(), Integer.parseInt(etReminder.getText().toString()));
+                        WeeklyWednesdayDatabaseHelper weeklyWednesdayDatabaseHelper = new WeeklyWednesdayDatabaseHelper(InputRepetitive.this);
+                        boolean success = weeklyWednesdayDatabaseHelper.addOne(weeklyWednesdayActivityModel);
+                        Intent intent = new Intent(InputRepetitive.this, ListActivity.class);
+                        startActivity(intent);
                         Toast.makeText(InputRepetitive.this, weeklyWednesdayActivityModel.toString(), Toast.LENGTH_SHORT).show();
+                        finish();
                     }else{
                         Toast.makeText(InputRepetitive.this, "Fields cannot be empty", Toast.LENGTH_SHORT).show();
-                        weeklyWednesdayActivityModel = new WeeklyWednesdayActivityModel("NaN", "NaN", "NaN", 0);
+//                        weeklyWednesdayActivityModel = new WeeklyWednesdayActivityModel("NaN", "NaN", "NaN", 0);
                     }
-                    WeeklyWednesdayDatabaseHelper weeklyWednesdayDatabaseHelper = new WeeklyWednesdayDatabaseHelper(InputRepetitive.this);
-                    boolean success = weeklyWednesdayDatabaseHelper.addOne(weeklyWednesdayActivityModel);
+
                 }
                 if(cbThursday.isChecked()){
                     WeeklyThursdayActivityModel weeklyThursdayActivityModel;
                     if (etActivity.length() != 0 && etLocation.length() != 0 && etTime.length() != 0 && etReminder.length() != 0) {
                         weeklyThursdayActivityModel = new WeeklyThursdayActivityModel(etActivity.getText().toString(), etLocation.getText().toString(), etTime.getText().toString(), Integer.parseInt(etReminder.getText().toString()));
+                        WeeklyThursdayDatabaseHelper weeklyThursdayDatabaseHelper = new WeeklyThursdayDatabaseHelper(InputRepetitive.this);
+                        boolean success = weeklyThursdayDatabaseHelper.addOne(weeklyThursdayActivityModel);
+                        Intent intent = new Intent(InputRepetitive.this, ListActivity.class);
+                        startActivity(intent);
                         Toast.makeText(InputRepetitive.this, weeklyThursdayActivityModel.toString(), Toast.LENGTH_SHORT).show();
+                        finish();
                     }else{
                         Toast.makeText(InputRepetitive.this, "Fields cannot be empty", Toast.LENGTH_SHORT).show();
-                        weeklyThursdayActivityModel = new WeeklyThursdayActivityModel("NaN", "NaN", "NaN", 0);
+//                        weeklyThursdayActivityModel = new WeeklyThursdayActivityModel("NaN", "NaN", "NaN", 0);
                     }
-                    WeeklyThursdayDatabaseHelper weeklyThursdayDatabaseHelper = new WeeklyThursdayDatabaseHelper(InputRepetitive.this);
-                    boolean success = weeklyThursdayDatabaseHelper.addOne(weeklyThursdayActivityModel);
+
                 }
                 if(cbFriday.isChecked()){
                     WeeklyFridayActivityModel weeklyFridayActivityModel;
                     if (etActivity.length() != 0 && etLocation.length() != 0 && etTime.length() != 0 && etReminder.length() != 0) {
                         weeklyFridayActivityModel = new WeeklyFridayActivityModel(etActivity.getText().toString(), etLocation.getText().toString(), etTime.getText().toString(), Integer.parseInt(etReminder.getText().toString()));
+                        WeeklyFridayDatabaseHelper weeklyFridayDatabaseHelper = new WeeklyFridayDatabaseHelper(InputRepetitive.this);
+                        boolean success = weeklyFridayDatabaseHelper.addOne(weeklyFridayActivityModel);
+                        Intent intent = new Intent(InputRepetitive.this, ListActivity.class);
+                        startActivity(intent);
                         Toast.makeText(InputRepetitive.this, weeklyFridayActivityModel.toString(), Toast.LENGTH_SHORT).show();
+                        finish();
                     }else{
                         Toast.makeText(InputRepetitive.this, "Fields cannot be empty", Toast.LENGTH_SHORT).show();
-                        weeklyFridayActivityModel = new WeeklyFridayActivityModel("NaN", "NaN", "NaN", 0);
+//                        weeklyFridayActivityModel = new WeeklyFridayActivityModel("NaN", "NaN", "NaN", 0);
                     }
-                    WeeklyFridayDatabaseHelper weeklyFridayDatabaseHelper = new WeeklyFridayDatabaseHelper(InputRepetitive.this);
-                    boolean success = weeklyFridayDatabaseHelper.addOne(weeklyFridayActivityModel);
+
                 }
                 if(cbSaturday.isChecked()){
                     WeeklySaturdayActivityModel weeklySaturdayActivityModel;
                     if (etActivity.length() != 0 && etLocation.length() != 0 && etTime.length() != 0 && etReminder.length() != 0) {
                         weeklySaturdayActivityModel = new WeeklySaturdayActivityModel(etActivity.getText().toString(), etLocation.getText().toString(), etTime.getText().toString(), Integer.parseInt(etReminder.getText().toString()));
+                        WeeklySaturdayDatabaseHelper weeklySaturdayDatabaseHelper = new WeeklySaturdayDatabaseHelper(InputRepetitive.this);
+                        boolean success = weeklySaturdayDatabaseHelper.addOne(weeklySaturdayActivityModel);
+                        Intent intent = new Intent(InputRepetitive.this, ListActivity.class);
+                        startActivity(intent);
                         Toast.makeText(InputRepetitive.this, weeklySaturdayActivityModel.toString(), Toast.LENGTH_SHORT).show();
+                        finish();
                     }else{
                         Toast.makeText(InputRepetitive.this, "Fields cannot be empty", Toast.LENGTH_SHORT).show();
-                        weeklySaturdayActivityModel = new WeeklySaturdayActivityModel("NaN", "NaN", "NaN", 0);
+//                        weeklySaturdayActivityModel = new WeeklySaturdayActivityModel("NaN", "NaN", "NaN", 0);
                     }
-                    WeeklySaturdayDatabaseHelper weeklySaturdayDatabaseHelper = new WeeklySaturdayDatabaseHelper(InputRepetitive.this);
-                    boolean success = weeklySaturdayDatabaseHelper.addOne(weeklySaturdayActivityModel);
+
                 }
                 if(cbSunday.isChecked()){
                     WeeklySundayActivityModel weeklySundayActivityModel;
                     if (etActivity.length() != 0 && etLocation.length() != 0 && etTime.length() != 0 && etReminder.length() != 0) {
                         weeklySundayActivityModel = new WeeklySundayActivityModel(etActivity.getText().toString(), etLocation.getText().toString(), etTime.getText().toString(), Integer.parseInt(etReminder.getText().toString()));
+                        WeeklySundayDatabaseHelper weeklySundayDatabaseHelper = new WeeklySundayDatabaseHelper(InputRepetitive.this);
+                        boolean success = weeklySundayDatabaseHelper.addOne(weeklySundayActivityModel);
+                        Intent intent = new Intent(InputRepetitive.this, ListActivity.class);
+                        startActivity(intent);
                         Toast.makeText(InputRepetitive.this, weeklySundayActivityModel.toString(), Toast.LENGTH_SHORT).show();
+                        finish();
                     }else{
                         Toast.makeText(InputRepetitive.this, "Fields cannot be empty", Toast.LENGTH_SHORT).show();
-                        weeklySundayActivityModel = new WeeklySundayActivityModel("NaN", "NaN", "NaN", 0);
+//                        weeklySundayActivityModel = new WeeklySundayActivityModel("NaN", "NaN", "NaN", 0);
                     }
-                    WeeklySundayDatabaseHelper weeklySundayDatabaseHelper = new WeeklySundayDatabaseHelper(InputRepetitive.this);
-                    boolean success = weeklySundayDatabaseHelper.addOne(weeklySundayActivityModel);
+
                 }
 
 //
@@ -205,13 +233,6 @@ public class InputRepetitive extends AppCompatActivity {
 //                Toast.makeText(InputOneTimeActivity.this, "Success= " + success, Toast.LENGTH_SHORT).show();
 //                Toast.makeText(InputOneTimeActivity.this, oneTimeActivityModel.toString(), Toast.LENGTH_SHORT).show();
 
-
-
-
-
-                Intent intent = new Intent(InputRepetitive.this, ListActivity.class);
-                startActivity(intent);
-                finish();
                 return;
             }
         });
